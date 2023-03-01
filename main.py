2d2fb2ff402ec5ff89305f92438f938a09a83545
Uniquemana <abolinskristians.com>
2023-03-01 18:12:43 +0200

    data processing updated

--- main.py
+++ main.py
@@ -6,47 +6,34 @@ import base64
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/app')
-def atapp():
-    co2 = 1037
-    airtemp = 21
-    airhumid = 35
-    return render_template("main.html",co2 = co2, airtemp = airtemp, airhumid = airhumid)
-
-
-conn = sqlite3.connect('data.db')
+# Connect to database
+conn = sqlite3.connect('/path/to/database.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS data
-             (timestamp DATETIME, co2 INTEGER, airTemp REAL, airHumidity REAL, led REAL)''')
-conn.commit()
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
 @app.route('/data', methods=['POST'])
-def receive_data():
-    co2 = request.form['co2']
-    airTemp = request.form['airTemp']
-    airHumidity = request.form['airHumidity']
-    led = request.form['led']
-    c.execute("INSERT INTO data (timestamp, co2, airTemp, airHumidity, led) VALUES (datetime('now'), ?, ?, ?)",
-              (co2, airTemp, airHumidity, led))
-    conn.commit()
-    return 'Data received'
+def data():
+    # Get POST data
+    co2 = request.form.get('co2')
+    airTemp = request.form.get('airTemp')
+    airHumidity = request.form.get('airHumidity')
 
-@app.route('/latest', methods=['GET'])
-def latest_data():
-    c.execute("SELECT * FROM data ORDER BY timestamp DESC LIMIT 1")
+    # Insert data into database
+    c.execute("INSERT INTO data (co2, airTemp, airHumidity) VALUES (?, ?, ?)", (co2, airTemp, airHumidity))
+    conn.commit()
+
+    return 'OK'
+
+@app.route('/latest')
+def latest():
+    # Get latest data from database
+    c.execute("SELECT * FROM data ORDER BY id DESC LIMIT 1")
     row = c.fetchone()
-    if row:
-        co2 = row[1]
-        airTemp = row[2]
-        airHumidity = row[3]
-        led = row[4]
-        return render_template('main.html', co2=co2, airTemp=airTemp, airHumidity=airHumidity,led=led)
-    else:
-        return 'No data'
+    co2 = row[1]
+    airTemp = row[2]
+    airHumidity = row[3]
+
+    # Render template with latest data
+    return render_template('latest.html', co2=co2, airTemp=airTemp, airHumidity=airHumidity)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
