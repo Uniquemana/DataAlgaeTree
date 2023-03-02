a95b865c4dd33cc3916c67eca69ed3e15c5fb5ba
Uniquemana <abolinskristians.com>
2023-03-02 20:27:08 +0200

    POST request and data visualisation

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
-from flask import Flask, request, render_template
-import sqlite3
-import matplotlib.pyplot as plt
-import io
-import base64
+from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 
-# Connect to database
-conn = sqlite3.connect('/path/to/database.db')
-c = conn.cursor()
+# Dictionary to store the data for each device
+device_data = {}
 
-@app.route('/data', methods=['POST'])
-def data():
-    # Get POST data
-    co2 = request.form.get('co2')
-    airTemp = request.form.get('airTemp')
-    airHumidity = request.form.get('airHumidity')
+@app.route('/<device_id>')
+def show_data(device_id):
+    if device_id not in device_data:
+        return 'No data available for device ' + device_id
+    else:
+        data = device_data[device_id]
+        return render_template('data.html', data=data, device_id=device_id)
 
-    # Insert data into database
-    c.execute("INSERT INTO data (co2, airTemp, airHumidity) VALUES (?, ?, ?)", (co2, airTemp, airHumidity))
-    conn.commit()
-
-    return 'OK'
-
-@app.route('/latest')
-def latest():
-    # Get latest data from database
-    c.execute("SELECT * FROM data ORDER BY id DESC LIMIT 1")
-    row = c.fetchone()
-    co2 = row[1]
-    airTemp = row[2]
-    airHumidity = row[3]
-
-    # Render template with latest data
-    return render_template('latest.html', co2=co2, airTemp=airTemp, airHumidity=airHumidity)
+@app.route('/', methods=['POST'])
+def receive_data():
+    data_str = request.data.decode('utf-8')
+    data_list = data_str.strip().split('\n')
+    for data in data_list:
+        device_id, co2, air_temp, air_humid, led = data.split()
+        # Update the data in the dictionary
+        device_data[device_id] = [(co2, air_temp, air_humid, led)]
+    return 'Data received successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
