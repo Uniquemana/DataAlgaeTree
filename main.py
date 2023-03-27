2fcc2f511676d955ec18f77859b219a74db3d607
Uniquemana <abolinskristians.com>
2023-03-27 16:09:18 +0300

    fixed posted data format

--- main.py
+++ main.py
@@ -22,14 +22,12 @@ def scan():
 @app.route('/<device_id>')
 def show_data(device_id):
     cur = conn.cursor()
-    cur.execute("SELECT * FROM device_data WHERE device_id = %s", (device_id,))
+    cur.execute("SELECT * FROM device_data WHERE device_id=%s", (device_id,))
     data = cur.fetchall()
-    cur.close()
-    if not data:
+    if len(data) == 0:
         return 'No data available for device ' + device_id
     else:
         return render_template('data.html', data=data, device_id=device_id)
-
 
 @app.route('/', methods=['POST'])
 def receive_data():
@@ -37,12 +35,12 @@ def receive_data():
     data_str = request.data.decode('utf-8')
     data_list = data_str.strip().split('\n')
     for data in data_list:
-        device_id, co2, air_temp, air_humid, leftwatertemp, rightwatertemp,leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm ,led = data.split()
+        device_id, co2, air_temp, air_humid, leftwatertemp, rightwatertemp, leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm, led = data.strip().split('\t')
         # Insert the data into the database
-        cur.execute("INSERT INTO device_data (device_id, co2, airtemp, airhumid, leftwatertemp, rightwatertemp, leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm, led) VALUES (%i, %f, %f, %f, %f, %f, %f, %f, %i, %i, %i)", (device_id, co2, air_temp, air_humid, leftwatertemp, rightwatertemp,leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm, led))
+        cur.execute("INSERT INTO device_data (device_id, co2, airtemp, airhumid, leftwatertemp, rightwatertemp, leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm, led) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (device_id, co2, air_temp, air_humid, leftwatertemp, rightwatertemp, leftheatertemp, rightheatertemp, leftheaterpwm, rightheaterpwm, led))
     conn.commit()
     cur.close()
-    return 'Data received and saved successfully'
+    return 'Data received successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
